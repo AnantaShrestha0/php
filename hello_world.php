2c02b30794ff00f27d1815655c8076ddda0e76c1
hello <helloworld@gmail.com>
2023-06-26 10:36:15 +0545

    added Dynamic table

--- hello_world.php
+++ hello_world.php
@@ -8,6 +8,77 @@
 <body>
     <?php
     echo "<h1>Hello world</h1> </br>";
+
+    //compair operator 
+    $Hariage="18";
+    $Ramage="18";
+    if($Hariage===$Ramage){
+        echo "They have equal age.</br>";
+    }else{
+        echo "They have different age.</br>";
+    }
+
+    $a=45;
+    if($a%2==0){
+        echo "Even";
+    }else{
+        echo "Odd";
+    }
+
+    $b=2;
+
+    // switch($b){
+    //     case 1:
+    //         echo "Namaste";
+    //         break;
+    //         case 2:
+    //             echo "Good Morning";
+    //             break;
+    //             case 3:
+    //                 echo "Bye"
+    //                 break;
+
+    //                 default:
+    //                 echo "mm";
+    //                 break;
+
+    // }
+
+    $counter=1;
+    while($counter<=10){
+        echo "hello world!</br>";
+        $counter++;
+    }
+
+
+    $tableData=
+    [
+        ['Johnny','johndoe@gmail.com'],
+        ['Mia','Mia@gmail.com'],
+        ['Danial','Denial@gmail.com']
+  
+
+    ];
+    echo "<table>";
+    echo "<tr><th>NAME</th><th>EMAIL</th>";
+
+    //LOOP THROUGH the table data array
+    $i=0;
+    while($i<count($tableData)){
+        echo "<tr>";
+ 
+        $j=0;
+        while($j<count($tableData[$i])){
+            echo "<td>".$tableData[$i][$j]."</td>";
+            $j++;
+        }
+        echo "</tr>";
+        $i++;
+
+    }
+
+    $i=0;
+
     ?>
 </body>
 </html>
